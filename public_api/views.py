715a3de19916c67ee3eedefd2e9dda7a8b6062f9
olavodotpy <olavoazevedo88@gmail.com>
2024-12-11 21:26:28 -0300

    upgrade view

--- public_api/views.py
+++ public_api/views.py
@@ -10,7 +10,7 @@ class get_data_endpoint_public(APIView):
     def get(self, request):
         try:
             # Use um timeout adequado para evitar bloqueios
-            response = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT', timeout=10)
+            response = requests.get('https://api.binance.us/api/v3/ticker/price?symbol=BTCUSDT', timeout=10)
             
             if response.status_code == 200:
                 return Response({"status": "success", "data": response.json()})
